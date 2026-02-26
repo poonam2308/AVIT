75cd7cd5f21c49e29ca193b4dddb7c14a6f11344
poonam2308 <poonamps1791@gmail.com>
2026-02-26 16:45:58 +0100

    imagewoof_route te

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 unset WANDB_MODE
-WANDB_MODE=online python main.py --config configs/imagenette.yaml
+#WANDB_MODE=online python main.py --config configs/imagenette.yaml
 
-#WANDB_MODE=online python main.py --config configs/imagewoof.yaml
+WANDB_MODE=online python main.py --config configs/imagewoof.yaml
 
 #WANDB_MODE=online python main.py --config configs/cifar10.yaml
